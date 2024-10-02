6ad9b4e35906b9bfe2b5fb00ca82385e1fcac216
GaiaOcean <86306468+GaiaOcean@users.noreply.github.com>
2024-10-02 13:07:13 -0300

    Update JVida_BGLL_Projeto_Model.cpp

--- JVida-BGLL/JVida_BGLL_Projeto_Model.cpp
+++ JVida-BGLL/JVida_BGLL_Projeto_Model.cpp
@@ -11,9 +11,18 @@ Nome dos integrantes:
 */
 
 #include "JVida_BGLL_Projeto_Model.h"
+
+#include <stdio.h>
 void inicializarMatriz60x60(){
 	for (int i = 0; i < 60; i++)
       for (int j = 0; j < 60; j++) 
         jdvMatriz[i][j] = '.'; // Define todos as celulas como vazias inicialmente
 }
 
+void perguntarDIm(int* dim){
+	
+	printf("Digite a dimensao da matriz(de 10 a 60): " );
+	scanf("%d",dim);
+	
+//	return *dim;
+}
